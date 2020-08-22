9a55de4fc17d7b83a513bd3cbd79b1a3a8351e86
Siraak_ <55248135+0xSiraak@users.noreply.github.com>
2020-08-23 01:21:15 +0200

    Create search.py

---
+++ search.py
@@ -0,0 +1,12 @@
+import os
+import pyfiglet
+from colorama import Fore, Back, Style
+
+r = pyfiglet.figlet_format("DB Searcher")
+print(Fore.CYAN + r)
+print(Fore.CYAN + "Developped by 0xSiraak on GitHub")
+
+user = input(Fore.GREEN + "\n[+] - Username, Email, IP, UUID : ")
+
+print(Fore.GREEN + "[+] - Results : \n")
+os.system("cat /databases/* | grep " + user)
